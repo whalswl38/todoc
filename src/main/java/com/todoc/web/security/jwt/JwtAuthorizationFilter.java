2976ec369e36c06efda258c03f5253a442bc9d68
HwangHaejung <o1o2o4o5@naver.com>
2024-05-08 15:01:48 +0900

    2024-05-08 황해정 회원가입, 로그인 기능 구현

---
+++ src/main/java/com/todoc/web/security/jwt/JwtAuthorizationFilter.java
@@ -0,0 +1,115 @@
+package com.todoc.web.security.jwt;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+// permitAll, login 요청 제외 요청 처리 // 사용자 인증, 인가를 처리해주는 필터
+@Slf4j
+@RequiredArgsConstructor
+public class JwtAuthorizationFilter extends OncePerRequestFilter 
+{	
+	@SuppressWarnings("unused")
+	private final ObjectMapper objectMapper;
+	@SuppressWarnings("unused")
+	private final JwtTokenProvider jwtTokenProvider;
+	
+	public JwtAuthorizationFilter(JwtTokenProvider jwtTokenProvider, ObjectMapper objectMapper)
+	{
+		this.jwtTokenProvider = jwtTokenProvider;
+		this.objectMapper = objectMapper;
+	}
+	
+    // 요청에서 쿠키를 추출해서, JWT 토큰을 담고 있는 쿠키를 찾아 JWT 토큰 문자열 또는 null 을 반환
+    public String extractJwtFromCookie(HttpServletRequest request) 
+    {
+    	log.info("extractJwtFromCookie");
+        Cookie[] cookies = request.getCookies();
+        
+        if (cookies != null) 
+        {
+            for (Cookie cookie : cookies)
+            {
+                if (JwtProperties.HEADER_STRING.equals(cookie.getName())) 
+                {
+                    return cookie.getValue();
+                }
+            }
+        }
+        
+        return null;
+    }
+    
+    // JWT 토큰을 쿠키에서 추출하고, 토큰이 유효한 경우 사용자 이름을 추출해서 security의 SecurityContext에 인증 정보를 등록
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException 
+    {
+    	log.info("doFilterInternal");
+        String token = extractJwtFromCookie(request);
+        
+        if (token != null && validateToken(token)) 
+        {
+            String username = getUsernameFromToken(token); // 토큰에서 사용자 이름 추출 로직 구현 필요
+            UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(username, null, Collections.singletonList(new SimpleGrantedAuthority("ROLE_USER")));
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+        
+        chain.doFilter(request, response);
+    }
+    
+    // 토큰 값 유효성 체크
+    public boolean validateToken(String token) 
+    {
+    	log.info("validateToken");
+        try 
+        {
+            JWT.require(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()))
+                .build()
+                .verify(token.replace(JwtProperties.TOKEN_PREFIX, ""));
+            
+            return true;
+        } 
+        catch (JWTVerificationException e) 
+        {
+        	log.error("[JwtAuthorizationFilter validateToken JWTVerificationException] ", e);
+            return false;
+        }
+    }
+    
+    // 토큰에서 추출한 사용자 이름(userEmail) 반환
+    public String getUsernameFromToken(String token) 
+    {
+    	log.info("getUsernameFromToken");
+        try 
+        {
+            DecodedJWT jwt = JWT.require(Algorithm.HMAC512(JwtProperties.SECRET.getBytes()))
+                                .build()
+                                .verify(token.replace(JwtProperties.TOKEN_PREFIX, ""));
+            
+            return jwt.getSubject();
+        } 
+        catch (Exception e) 
+        {
+            log.error("[JwtAuthorizationFilter getUsernameFromToken Exception] ", e);
+            return null;
+        }
+    }
+}
--- src/main/java/com/todoc/web/service/UserService.java
+++ src/main/java/com/todoc/web/service/UserService.java
@@ -1,19 +1,133 @@
 package com.todoc.web.service;
 
+import java.util.HashMap;
+import java.util.Random;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.todoc.web.dao.UserDao;
+import com.todoc.web.dto.Clinic;
+import com.todoc.web.dto.Institution;
+import com.todoc.web.dto.Pharmacy;
 import com.todoc.web.dto.User;
+
+import net.nurigo.java_sdk.api.Message;
 
 @Service
 public class UserService 
 {
 	@Autowired
-	private UserDao usermapper;
+	private UserDao userDao;
 	
-	public User read (String userId) throws Exception
+	private PasswordEncoder passwordEncoder;
+		
+	public boolean checkPassword(String rawPassword, String encodedPassword) 
 	{
-		return usermapper.read(userId);
+	    return passwordEncoder.matches(rawPassword, encodedPassword);
+	}
+	
+	// 리프레쉬 토큰으로 회원 조회
+	public User findByRefreshToken(String userRefreshToken)
+	{
+		return userDao.findByRefreshToken(userRefreshToken);
+	}
+	
+	// 리프레쉬 토큰 업데이트
+	public void refreshTokenUpdate(User user) 
+	{
+	    userDao.refreshTokenUpdate(user);
+	}
+	
+	// 병원 리프레쉬 토큰 업데이트
+	public void clinicRefreshTokenUpdate(Clinic clinic)
+	{
+		userDao.clinicRefreshTokenUpdate(clinic);
+	}
+	
+	// 약국 리프레쉬 토큰 업데이트
+	public void PharmRefreshTokenUpdate(Pharmacy pharmacy)
+	{
+		userDao.PharmRefreshTokenUpdate(pharmacy);
+	}
+	
+	// 이메일로 회원 확인
+	public User findByEmail(String userEmail)
+	{
+		return userDao.findByEmail(userEmail);
+	}
+	
+	// 이메일로 병원 회원 확인
+	public Clinic findClinicEmail(String userEmail)
+	{
+		return userDao.findClinicEmail(userEmail);
+	}
+	
+	// 이메일로 약국 회원 확인
+	public Pharmacy findPharmEmail(String userEmail)
+	{
+		return userDao.findPharmEmail(userEmail);
+	}
+	
+	// 회원가입
+	public int userInsert(User user)
+	{
+		return userDao.userInsert(user);
+	}
+	
+	// 문자 인증
+	public String sendSMS(String to) throws Exception
+	{	
+		String api_key = "NCSFHBPPFH4YKLGD";
+		String api_secret_key = "XUNKCP6IOIIVUWRY18PAWSRZKBXMBXV1";
+
+		Message coolsms = new Message(api_key, api_secret_key);
+		
+		Random rand = new Random();
+		
+		String numStr = "";
+		
+		// 인증번호 랜덩값
+		for(int i=0 ; i<6 ; i++)
+		{
+			String ran = Integer.toString(rand.nextInt(10));
+			numStr += ran;
+		}
+		
+		// 문자 서비스
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put("to", to); // 수신전화번호
+		params.put("from", "010-2580-3668"); // 발신전화번호
+		params.put("type", "sms");
+		params.put("text", "[todoc] 인증번호 [ "+ numStr + " ] 타인에게 절대 알려주지 마세요.");
+		
+		coolsms.send(params); // 문자 전송
+		
+		return numStr;
+	}
+	
+	// 병의원, 약국 회원가입시 요양기관 가입여부 확인
+	public Institution findInstitution(String institutionNum)
+	{
+		return userDao.findInstitution(institutionNum);
+	}
+	
+	// 이메일 중복확인
+	public int checkEmail(String userEmail)
+	{
+		return userDao.checkEmail(userEmail);	
+	}
+	
+	// 약국 회원가입
+	public int insertPharmacy(Pharmacy pharmacy)
+	{
+		return userDao.insertPharmacy(pharmacy);
+	}
+	
+	// 병원 회원가입
+	public int insertClinic(Clinic clinic)
+	{
+		return userDao.insertClinic(clinic);
 	}
 }

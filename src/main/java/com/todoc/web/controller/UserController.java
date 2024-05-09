9f22f534146fa5fbb7455b755fef58859c8dea5c
HwangHaejung <o1o2o4o5@naver.com>
2024-05-09 16:42:22 +0900

    2024-05-09 황해정 로그인, 회원가입 기능 구현

--- src/main/java/com/todoc/web/controller/UserController.java
+++ src/main/java/com/todoc/web/controller/UserController.java
@@ -1,41 +1,206 @@
 package com.todoc.web.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.validation.Valid;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.todoc.web.dto.Clinic;
+import com.todoc.web.dto.Pharmacy;
 import com.todoc.web.dto.User;
+import com.todoc.web.security.dto.SignUpDto;
 import com.todoc.web.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping
+@RequiredArgsConstructor
 public class UserController 
-{
-	@Autowired
-	private UserService userService;
+{	
+	private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
-	
-	@GetMapping("/login")
-	public String userRead(ModelMap model, String userId) throws Exception{				
+	// 로그인 페이지
+    @GetMapping("/login-page")
+    public String loginPage( ) 
+    {
+        return "login/login";
+    }
+    
+    // 병원, 약국 회원가입 페이지
+    @GetMapping("/medical-register-page")
+    public String medicalRegisterPage(Model model) 
+    {
+    	model.addAttribute("signUpDto", new SignUpDto());
+    	
+        return "login/medicalRegister";
+    }
+    
+    // 병원, 약국 회원 회원가입 기능
+	@PostMapping("/medicalSign")
+	public String medicalSign(@Valid SignUpDto signUpDto, BindingResult bindingResult, Model model)
+	{	
+		if(bindingResult.hasErrors())
+		{
+			model.addAttribute("signUpDto", signUpDto);
+			return "login/medicalRegister";
+		}
 		
-		userId = "test1";
-		User user = userService.read(userId);
+		Clinic clinic  = new Clinic();
+		Pharmacy pharmacy = new Pharmacy();
+		String type = "";
 		
-		
-		model.addAttribute("user", user);
-		
-		return "login/login";	
+		if(signUpDto.getUserType().equals("병의원"))
+		{
+			try
+			{
+				clinic.setClinicInstinum(signUpDto.getInstitutionNum());
+				clinic.setClinicRegnum(signUpDto.getRegNum());
+				clinic.setClinicPhone(signUpDto.getUserPhone());
+				clinic.setClinicName(signUpDto.getInstitutionName());
+				clinic.setClinicSubject(signUpDto.getSubject());
+				clinic.setClinicSymptom(signUpDto.getSymptop());
+				clinic.setUserEmail(signUpDto.getUserEmail());
+				clinic.setUserPwd(passwordEncoder.encode(signUpDto.getUserPwd()));
+				clinic.setClinicDay(signUpDto.getDayOn());
+				clinic.setClinicTime(signUpDto.getDayTime());
+				clinic.setClinicDayoff(signUpDto.getDayOff());
+				clinic.setClinicZipcode(signUpDto.getZipcode());
+				clinic.setClinicAddr(signUpDto.getAddr());
+
+				if(signUpDto.getContactType().contains("대면"))
+				{
+					type = "C";
+				}
+				else if(signUpDto.getContactType().contains("비대면"))
+				{
+					type = "U";
+				}
+				else if(signUpDto.getContactType().contains(","))
+				{
+					type = "C, U";
+				}
+
+				clinic.setUserEmail(signUpDto.getUserEmail());
+				clinic.setClinicContactFlag(type);
+				clinic.setClinicDetail(signUpDto.getDetail());
+				clinic.setClinicDoctor(signUpDto.getUserName());
+				clinic.setClinicFax(signUpDto.getFaxNum());
+				clinic.setClinicCareer(signUpDto.getCareer());
+				clinic.setClinicBreak(signUpDto.getBreakTime());
+				clinic.setClinicNight(signUpDto.getNight());
+				clinic.setClinicWeekend(signUpDto.getWeekend());
+
+				if(userService.insertClinic(clinic) > 0)
+				{
+					return "redirect:/login-page";
+				}
+				
+				model.addAttribute("signUpDto", signUpDto);
+				return "login/medicalRegister";
+			
+			}
+			catch(Exception e)
+			{
+				log.error("sign Exception");
+				model.addAttribute("errorMessage", e.getMessage());
+				
+				return "login/medicalRegister";
+			}
+		}
+		else
+		{
+			try
+			{
+				pharmacy.setUserEmail(signUpDto.getUserEmail());
+				pharmacy.setPharmacyInstinum(signUpDto.getInstitutionNum());
+				pharmacy.setPharmacyRegnum(signUpDto.getRegNum());
+				pharmacy.setPharmacyPhone(signUpDto.getInstitutionNum());
+				pharmacy.setPharmacyName(signUpDto.getInstitutionName());
+				pharmacy.setPharmacistName(signUpDto.getUserName());
+				pharmacy.setUserPwd(passwordEncoder.encode(signUpDto.getUserPwd()));
+				pharmacy.setPharmacyDayoff(signUpDto.getDayOff());
+				pharmacy.setPharmacyTime(signUpDto.getDayTime());
+				pharmacy.setPharmacyZipcode(signUpDto.getZipcode());
+				pharmacy.setPharmacyAddr(signUpDto.getAddr());
+				pharmacy.setPharmacyFax(signUpDto.getFaxNum());
+				pharmacy.setPharmacyCareer(signUpDto.getCareer());
+				
+				if(userService.insertPharmacy(pharmacy) > 0)
+				{
+					return "redirect:/login-page";
+				}
+				
+				model.addAttribute("signUpDto", signUpDto);
+				return "login/medicalRegister";
+			}
+			catch(Exception e)
+			{
+				log.error("sign Exception : " + e);
+				model.addAttribute("errorMessage", e.getMessage());
+				
+				return "login/medicalRegister";
+			}
+		}
 	}
-	
-	@GetMapping("/test")
-	public String userRead() throws Exception{				
+    
+    // 일반 회원 회원가입 페이지
+    @GetMapping("/register-page")
+    public String registerPage(Model model) 
+    {
+        model.addAttribute("user", new User());
+        
+    	return "login/register";
+    }
+    
+    // 일반 회원 회원가입 기능
+	@PostMapping("/sign")
+	public String sign(@Valid User user, BindingResult bindingResult, Model model)
+	{
+		if(bindingResult.hasErrors())
+		{
+			model.addAttribute("user", user);
+			return "login/register";
+		}
 		
-		return "/test";	
-	}
+		User signUser = new User();
+		
+		try
+		{
+			signUser.setUserEmail(user.getUserEmail());
+			signUser.setUserPwd(passwordEncoder.encode(user.getUserPwd()));
+			signUser.setUserName(user.getUserName());
+			signUser.setUserPhone(user.getUserPhone());
+			signUser.setUserIdentity(user.getUserIdentity());
+			
+			if(userService.userInsert(signUser) > 0)
+			{
+				return "redirect:/login-page";
+			}
+			
+			model.addAttribute("user", user);
+			return "login/register";
+		}
+		catch(Exception e)
+		{
+			log.error("sign Exception");
+			model.addAttribute("errorMessage", e.getMessage());
+			
+			return "login/register";
+		}
+	}	
 	
+	@PostMapping("/admin/oo")
+	public String tesst()
+	{
+		return "메롱";
+	}
 }
--- src/main/java/com/todoc/web/dto/User.java
+++ src/main/java/com/todoc/web/dto/User.java
@@ -1,20 +1,48 @@
 package com.todoc.web.dto;
 
+
+import javax.validation.constraints.*;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
 import lombok.Data;
 
 @Data
-public class User {
-	
-	private String userId;
-	private String userPwd;
-	private String userPhone;
+public class User
+{
+	@NotBlank(message="이메일은 필수 입력 값입니다.")
+	@Email(message="이메일 형식이 올바르지 않습니다.")
 	private String userEmail;
-	private String userEmailFlag;
+	
+	@NotEmpty(message="비밀번호는 필수 입력 값입니다.")
+	@Size(min=4, max=10)
+	@Pattern(regexp="^(?=.*[A-Za-z])(?=.*\\d)[A-Za-z\\d\\w\\W]{8,}$", message="비밀번호는 영문 대소문자, 숫자를 포함 8자리이상이어야 합니다.")
+	private String userPwd;
+	
+	@NotEmpty(message="이름은 필수 입력 값입니다.")
+	@Size(min=2, message="이름은 2자 이상 입력하셔야합니다.")
 	private String userName;
-	private String userStatus;
-	private String userFlag;
+	
+	private String userIdentity;
+	
+	@NotEmpty(message="전화번호는 필수 입력 값입니다.")
+	private String userPhone;
+	
+	private String userPhoneFlag;
+	
 	private String userRegdate;
-	private String userZipcode;
-	private String userAddr1;
-	private String userAddr2;
+	
+	private String userStatus;
+	
+	private String userType;
+	
+	private String socialType;
+	
+	private String userRefreshToken;
+	
+	public User encodePassword(PasswordEncoder passwordEncoder) 
+	{
+		this.userPwd = passwordEncoder.encode(this.userPwd);
+		return this;
+	} 
 }

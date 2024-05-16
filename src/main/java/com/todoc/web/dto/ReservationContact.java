b6d03a20a0f8f7c05ddcf99b002f126b87993030
YangHeeju <neeioo0503@gmail.com>
2024-05-16 17:37:24 +0900

    05-16 대면병원 리스트 상세페이지 예약

---
+++ src/main/java/com/todoc/web/dto/ReservationContact.java
@@ -0,0 +1,29 @@
+package com.todoc.web.dto;
+
+import lombok.Data;
+
+@Data
+public class ReservationContact {
+
+	private long reservationSeq;
+	private String userEmail;
+	private String clinicInstinum;
+	private String reservationDate;
+	private String reservationTime;
+	private String reservationSymptom;
+	private String reservationFlag;
+	private String regdate;
+	
+	
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+}

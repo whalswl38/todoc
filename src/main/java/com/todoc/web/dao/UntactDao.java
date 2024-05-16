d2a6a76db478e6c5999d011bc74529db1ab17c07
kei01118 <kei01118@gmail.com>
2024-05-16 13:38:27 +0900

    2024-05-16 조민지 merge 테스트

---
+++ src/main/java/com/todoc/web/dao/UntactDao.java
@@ -0,0 +1,22 @@
+package com.todoc.web.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.todoc.web.dto.Reserve;
+import com.todoc.web.dto.Untact;
+
+@Mapper
+public interface UntactDao {
+	List<Untact> subjectList(Untact untact);
+	
+	Untact selectClinicDetail(Untact untact);
+	
+	List<Untact> reviewList(Untact untact);
+
+	int subjectListCount(Untact untact);
+
+	int insertReservation(Reserve rsve);
+
+}
